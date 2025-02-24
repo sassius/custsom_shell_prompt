ce4989473651ca3b0e349f6a7b073000e4918b7e
sassius <145041644+sassius@users.noreply.github.com>
2025-02-25 01:46:38 +0530

    k

--- app/main.js
+++ app/main.js
@@ -2,7 +2,7 @@ const { spawn } = require("child_process");
 const readline = require("readline");
 const fs = require("fs");
 const path = require("path");
-// const { isStringObject } = require("util/types");
+// const { isStringObject } = require("util/types")
 
 const rl = readline.createInterface({
   input: process.stdin,
